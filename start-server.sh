71a0f8de55878cc88893a0a38ab069c2ef3ecb33
Yuhau Lin <lyuhau@gmail.com>
2023-04-06 14:09:12 -0400

    arkmanager.cfg: Add `arkbackuparchive` option

--- start-server.sh
+++ start-server.sh
@@ -2,4 +2,7 @@
 
 ./build.sh
 
+mkdir -p ARK-Backups
+chown -R 1000:1000 ARK-Backups
+
 docker-compose up -d
--- stop-server.sh
+++ stop-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec ark-ded arkmanager stop
+docker exec ark-ded bash -c 'arkmanager backup && arkmanager stop'
