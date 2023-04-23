8b79611d49bf0ec22d260007334febc0c2267ee4
Yuhau Lin <lyuhau@gmail.com>
2023-04-23 17:00:09 +0000

    Updating scripts

---
+++ backup-server.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker exec ark-ded bash -c 'arkmanager saveworld && arkmanager backup'
--- stop-server.sh
+++ stop-server.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker exec ark-ded bash -c 'arkmanager backup && arkmanager stop'
+docker exec ark-ded bash -c 'arkmanager saveworld && arkmanager backup && arkmanager stop'
+
